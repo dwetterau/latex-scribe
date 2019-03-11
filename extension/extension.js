d4ed43dd0853a72b65b8b304ffeab4a9feb4d607
David Wetterau <davidw@dropbox.com>
2019-03-10 17:04:37 -0700

    resize the extension

--- extension/extension.js
+++ extension/extension.js
@@ -11,12 +11,13 @@ function init() {
         "left: 50px;" +
         "bottom: 0px;" +
         "background: rgba(0, 0, 0, 0.1);" +
-        "height: 179px;" +
-        "width: 306px;";
+        "height: 186px;" +
+        "width: 506px;" +
+        "z-index: 100;";
 
     // Note: uncomment for development
-    iframe.src = "https://localhost:8080/";
-    //iframe.src = "https://latex.davidw.tech/";
+    // iframe.src = "https://localhost:8080/";
+    iframe.src = "https://latex.davidw.tech/";
     textArea.parentElement.appendChild(iframe);
 
     window.addEventListener("message", function(e) {
